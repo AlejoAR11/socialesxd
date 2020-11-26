ee6af63c78b93f8ed8789af77ae235e4411c0ac6
Juan Pablo Roldán Patiño <56945649+JuanPabloRP@users.noreply.github.com>
2020-11-26 17:38:47 -0500

    referendo gg

--- src/Pages/referendo.js
+++ src/Pages/referendo.js
@@ -3,6 +3,7 @@ import { Link, NavLink } from "react-router-dom";
 import Nav from "../components/nav.js";
 import style from "../Style/inicio.css";
 import Footer from "../components/footer.js";
+import Referendo from "../Images/referendo.jpg"
 
 class Referendo extends React.Component {
   render() {
@@ -17,54 +18,69 @@ class Referendo extends React.Component {
             <h2 class="titulos"> Referendo</h2>
 
             <p class="texto">
-              <a href="https://www.funcionpublica.gov.co/eva/gestornormativo/norma.php?i=330.">{" "}
-              Según la ley 134, Artículo 3. </a> “Es la convocatoria que se hace al
-              pueblo para que apruebe o rechace un proyecto de norma jurídica o
-              derogue o no una norma ya vigente.” Al igual que en el caso de las
-              iniciativas legislativas o normativas, se beben seguir los
-              siguientes pasos:
+              <a href="https://www.funcionpublica.gov.co/eva/gestornormativo/norma.php?i=330.">
+                {" "}
+                Según la ley 134, Artículo 3.{" "}
+              </a>{" "}
+              “Es la convocatoria que se hace al pueblo para que apruebe o
+              rechace un proyecto de norma jurídica o derogue o no una norma ya
+              vigente.” Al igual que en el caso de las iniciativas legislativas
+              o normativas, se beben seguir los siguientes pasos:
               <br />
               <br />
+              <br />
+              <br />
+              Cualquier⠀ciudadano puede solicitar a la Registraduría que sea
+              inscrito como promotor del referendo. Una vez inscrito, la
+              Registraduría entregará al⠀promotor o promotores del referendo un
+              formulario para la recolección de apoyos, en el que en el que
+              deberá aparecer⠀indicado el número de⠀firmas que es necesario para
+              inscribir la iniciativa y un resumen del contenido de la propuesta
+              para que cualquier interesado pueda leerla⠀antes de manifestar su
+              apoyo con su firma.<br/> Una vez⠀entregados los formularios de
+              recolección de apoyos a los promotores del referendo, contarán con
+              seis meses para recolectar las⠀firmas⠀de apoyo, plazo que puede
+              ser prorrogado⠀en caso de fuerza mayor o caso fortuito, hasta por
+              tres meses más, en la forma⠀y por el tiempo que señale el Consejo
+              Nacional Electoral.⠀El referendo puede ser tanto constitucional
+              como legal.⠀El referendo deberá realizarse dentro
+              de los seis meses siguientes a la⠀presentación de la solicitud.
+              <br />
+              <br />
+             <center><strong> Resumen </strong> </center>
+             <br/>
               <ul>
-                Se debe construir un comité promotor y con un apoyo equivalente
-                al 0.5% del censo electoral.
+              <li>              
+                  Solicitar a la registraduria que sea inscrito como promotor de un referendo
+                </li>
+                <li>
+                  Inscribir la iniciativa de referendo para proceder a
+                  recolectar las firmas que lo respalden que tienen que ser
+                  equivalentes al 5% del censo electoral.
+                </li>
+                <li>              
+                  Se debe construir un comité promotor y con un apoyo
+                  equivalente al 0.5% del censo electoral.
+                </li>
+                
+                <li>Ser revisado y aprobado por el congreso.</li>
               </ul>
-              <ul>
-                Inscribir la iniciativa de referendo para proceder a recolectar
-                las firmas que lo respalden que tienen que ser equivalentes al
-                5% del censo electoral.
-              </ul>
-              <ul>Ser revisado y aprobado por el congreso.</ul>
-              <br />
-              Una forma de entender el funcionamiento del referendo con un
-              ejemplo:
               <br />
               <br />
-              Se aprueba una ley en la que aumenta la edad de pensión para todas
-              las personas en 5 años. El pueblo, al ver que esta decisión va en
-              contra de sus intereses rechaza esta ley y se unen bajo el
-              mecanismo de referendo para reunir firmas y, así obligar al
-              congreso a juntarse para modificar o revocar la ley. O en un caso
-              contrario se rechaza una propuesta en la que se quiere bajar la
-              edad de pensión y el congreso la rechaza, el pueblo también tiene
-              el poder para juntarse, reunir firmas y hacer que el congreso
-              rectifique esta decisión, al considerar que no están actuando en
-              pro a sus intereses.
-              <br />
-              <br />
-              Es convocado por el pueblo, para esto se deben alcanzar firmas
-correspondientes al 5% del censo electoral inscrito. El Congreso es el encargado de
 
-14
+             Se hace al pueblo para que apruebe o rechace un proyecto de norma jurídica o derogue o no una norma vigente. Para poder participar del refrendo debera llenar el formulario correspondiente.
 
-validar dichas firmas y una vez pase el umbral se dará paso a las votaciones, en las
-cuales se debe tener una participación de al menos ¼ (25%) de la población registrada
-en el censo electoral para ser tomado como válido.
-Se debe ser mayor de edad para participar tanto en el recaudo de firmas de apoyo
-como para votar en la consulta popular en cuestión.
+            <br/>
+            <br/>
+
+             <center>       
+                <img src={Referendo} alt="..." id="referendo" />{" "}
+            </center>
+            
             </p>
+          
 
-            <p class="texto"></p>
+          
           </div>
         </div>
 
